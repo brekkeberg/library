2d79a817683d4416d0cae45444dce3deb2f7c8cc
brekkeberg <brekke.berg@gmail.com>
2023-10-12 16:02:16 -0700

    mvp to capture input from forms and display on screen

--- library.js
+++ library.js
@@ -4,15 +4,41 @@ const bookPages = document.querySelector("#pages")
 const bookReadBook = document.querySelector("#readIt")
 const buttonSubmit = document.querySelector("#buttonSubmit")
 
-console.log(bookTitle)
-console.log(bookAuthor)
-console.log(bookPages)
-console.log(bookReadBook)
-console.log(buttonSubmit)
+const outputSide = document.querySelector(".outputSide")
+
 
 buttonSubmit.addEventListener('click', testfunc);
 
 function testfunc(){
-    console.log("hello world")
-    console.log(bookTitle.value)
+    const book1 = new Book( bookTitle.value,
+                            bookAuthor.value,
+                            bookPages.value,
+                            bookReadBook.value)
+    book1.addBook()
+
 }
+
+
+
+
+function Book(title, author, pages, readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.addBook = function(){
+        let bookContainer = document.createElement('div');
+        bookContainer.classList.add('bookContainer');
+        bookContainer.style.cssText = "background-color: white; border-bottom: 2px solid gray;";
+        outputSide.appendChild(bookContainer);
+        bookContainer.innerText =   "Title: " + title +
+                                    " Author: " + author +
+                                    " Pages: " + pages +
+                                    " Have you read it? " + readStatus;
+        
+    }
+}
+/*
+const book1 = new Book("Steve", "asdf", 123123, "yes");
+book1.addBook()
+*/
