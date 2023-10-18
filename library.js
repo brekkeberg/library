f09bd421584f1b3bb4ea4cd4d57043c63c0c3930
brekkeberg <brekke.berg@gmail.com>
2023-10-17 20:53:57 -0700

    fixed delete button error, all cards display based on modifications to library array

--- library.js
+++ library.js
@@ -5,16 +5,13 @@ const formInputReadStatus = document.querySelector("#readIt");
 const buttonAddNewBook = document.querySelector("#buttonSubmit");
 const outputSide = document.querySelector(".outputSide")
 
-let library = [];
+let libraryArray = [];
 let bookCard;
 let titleContainer;
 let authorContainer;
 let pagesContainer;
 let readStatusContainer;
 let newBook;
-let bookCardDeleteButton;
-let libraryIndex;
-let bookID = 0;
 
 class Book {
     constructor(title, author, pages, readStatus, bookID) {
@@ -26,97 +23,112 @@ class Book {
     }
 }
 
-function createBookObject(){
+function createBook(){
     newBook = new Book( title = formInputTitle.value,
                         author = formInputAuthor.value,
                         pages = formInputPages.value,
                         readStatus = formInputReadStatus.value,
-                        bookID = bookID,
+                        bookID = libraryArray.length,
                         );
 }
 
-function addBookObjectToLibrary(){
-    library.push(newBook);
+function addBookToLibraryArray(){
+    libraryArray.push(newBook);
 }
 
-function incrementBookIDNumber(){
-    bookID ++;
+function clearAllCards(){
+    while (outputSide.firstChild) {
+        outputSide.removeChild(outputSide.firstChild);
+      }
 }
 
-function createBookCard(){
+function displayLibrary(){
+    for (let i = 0; i < libraryArray.length; i++){
+        renderBookCard();
+        addBookObjectDataToBookCard(libraryArray[i]);
+        assignDataAttributeToBookCard(libraryArray[i]);
+        addDeleteButtonToCard();
+    }
+}
+
+function clearForm(){
+    formInputTitle.value = "";
+    formInputAuthor.value = "";
+    formInputPages.value = "";
+}
+
+function renderBookCard(){
     bookCard = document.createElement('div');
     bookCard.style.cssText =   "background-color: white;" +
                                 "box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;" +
                                 "padding: 5px 0px 5px 0px;";
     outputSide.appendChild(bookCard);
 
+
     titleContainer = document.createElement('div');
     authorContainer = document.createElement('div');
     pagesContainer = document.createElement('div');
     readStatusContainer = document.createElement('div');
-    bookIDNumberContainer = document.createElement('div');
-    bookIDNumberContainer.classList.add('bookID');
 
     bookCard.appendChild(titleContainer);
     bookCard.appendChild(authorContainer);
     bookCard.appendChild(pagesContainer);
     bookCard.appendChild(readStatusContainer);
-    bookCard.appendChild(bookIDNumberContainer);
 }
 
-function addBookObjectDataToBookCard(){
-    titleContainer.innerText = newBook.title;
-    authorContainer.innerText = newBook.author;
-    if (newBook.pages != ""){
-        pagesContainer.innerText =  "Pages: " + newBook.pages;
+function addBookObjectDataToBookCard(book){
+    titleContainer.innerText = book.title;
+    authorContainer.innerText = book.author;
+    if (book.pages != ""){
+        pagesContainer.innerText =  "Pages: " + book.pages;
     }
-    if (newBook.readStatus === "Yes"){
+    if (book.readStatus === "Yes"){
         readStatusContainer.innerHTML = "<span class='material-symbols-outlined'>check_circle</span>"
     } else{
         readStatusContainer.innerHTML = "<span class='material-symbols-outlined'>cancel</span>"
     }
-    bookIDNumberContainer.innerText = bookID;
+}
+
+function assignDataAttributeToBookCard(book){
+    bookCard.setAttribute('data-ID', book.bookID);
 }
 
 function addDeleteButtonToCard(){
-    bookCardDeleteButton = document.createElement('button');
+    let bookCardDeleteButton = document.createElement('button');
     bookCardDeleteButton.classList.add('deleteButton');
     bookCard.appendChild(bookCardDeleteButton);
     bookCardDeleteButton.innerText = "Remove";
-    bookCardDeleteButton.addEventListener("click", ()=> {
-        bookCardDeleteButton.parentNode.remove();
-    })
-}
-let tester = 0
-function addDeleteButtonFunctionality(){
-    let allDeleteButtons = document.querySelectorAll(".deleteButton");
-    allDeleteButtons.forEach((button) => {
-        button.removeEventListener("click");
-        button.addEventListener("click", () => {
-            console.log("library length: " + library.length);
-            console.log(tester);
-            tester ++;
-
-            /* grabs current book ID 
-            let domBookID = button.parentNode.querySelector('.bookID').innerText;
-            /* for (let i = 0; i < library.length; i ++){
-                console.log(library[i].bookID)
-                if (library[i].bookID = domBookID){
-                    libraryIndex = i;
-                }
-            }
-            library.splice(libraryIndex, 1); */
-            button.parentNode.remove(); 
-        })
+    bookCardDeleteButton.addEventListener('click', ()=>{
+        //removes the correct object from bookArray by locating based on bookID
+        let currentBookID = bookCardDeleteButton.parentNode.dataset.id;
+        index = locateIndexInLibraryArray(currentBookID);
+        libraryArray.splice(index, 1);
+        clearAllCards();
+        displayLibrary();
     })
 }
 
+function toggleReadStatus(){
+
+}
+
+function locateIndexInLibraryArray(currentBookID){
+    let index;
+    for (let i = 0; i < libraryArray.length; i++){
+        if (libraryArray[i].bookID == currentBookID){
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
 
 buttonAddNewBook.addEventListener('click', () => {
-    createBookObject();
-    addBookObjectToLibrary();
-    createBookCard();
-    addBookObjectDataToBookCard();
-    addDeleteButtonToCard();
-    incrementBookIDNumber();
+    createBook();
+    addBookToLibraryArray();
+    clearAllCards();
+    displayLibrary();
+    clearForm();
 });
+
+
